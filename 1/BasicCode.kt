1c1d2621fac409d8c666cbfc59628fccc4838ad6
Abhishek kumar <abhishekhzb8@gmail.com>
2024-05-04 21:17:00 +0530

    Update BasicCode.kt

--- 1/BasicCode.kt
+++ 1/BasicCode.kt
@@ -1,1 +1,13 @@
+*****************************************************
+//for Loop
+fun main() {
+   val str = "Abhishek"
+   val ch = str.toCharArray()
+   
+   for(i in ch){
+      print(i) 
+   }
+}
 
+output : A b h i s h e k
+******************************************************
