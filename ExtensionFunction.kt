be54e46e4a632ca3fd5b141fe69df9d1abe899b7
Abhishek kumar <abhishekhzb8@gmail.com>
2024-05-07 23:35:01 +0530

    Update ExtensionFunction.kt

--- ExtensionFunction.kt
+++ ExtensionFunction.kt
@@ -8,7 +8,7 @@ fun main() {
 fun Int.square():Int{
     return this * this
 }
-/............. String ................
+//............. String ................
 
 fun main() {
   val name = "Abhishek"
@@ -18,4 +18,24 @@ fun main() {
 
 fun String.addTitleKumar():String{
     return "$this Kumar"
+}
+//...............................
+
+fun main(args: Array<String>) {
+    val nameM = "Abhishek";
+    val nameF = "Alice"
+    
+    val result = nameM.addPrefix("male")
+    println(result)
+     
+    val result2 = nameF.addPrefix("female")
+    println(result2)
+}
+
+fun String.addPrefix(gender :String):String{
+  return when(gender){
+    "male" -> "Mr $this"
+    "female" -> "Miss $this"
+     else -> this
+  }
 }
