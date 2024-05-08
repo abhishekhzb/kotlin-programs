19415a621a31e7f85f9b1542925d185df154879b
Abhishek kumar <abhishekhzb8@gmail.com>
2024-05-08 20:15:46 +0530

    Update ReverseArray.kt

--- Array/ReverseArray.kt
+++ Array/ReverseArray.kt
@@ -1,9 +1,20 @@
-//...................................................................
-
 fun main() {
-    val numbers = arrayOf(3, 1, 4, 1, 5, 9, 2, 6, 5)
-    numbers.sort()
-    println("Sorted array in place: ${numbers.joinToString()}")
+    val array = intArrayOf(1, 2, 3, 4, 5)
+    println("Original Array: ${array.joinToString()}")
+
+    reverseArray(array)
+    println("Reversed Array: ${array.joinToString()}")
 }
 
-//....................................................................
+fun reverseArray(array: IntArray) {
+    var start = 0
+    var end = array.size - 1
+
+    while (start < end) {
+        val temp = array[start]
+        array[start] = array[end]
+        array[end] = temp
+        start++
+        end--
+    }
+}
