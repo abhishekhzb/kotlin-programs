1ecc54265f0567cadd26102be205ab261fadf2e0
Abhishek kumar <abhishekhzb8@gmail.com>
2024-05-08 14:26:50 +0530

    Update NullSafty.kt

--- Null Safety/NullSafty.kt
+++ Null Safety/NullSafty.kt
@@ -1,1 +1,12 @@
+Safe Calls (?.):  
+val length: Int? = nullableString?.length
 
+Elvis Operator (?:): 
+val nonNullString: String = nullableString ?: "Default Value"
+
+Safe Cast Operator (as?): 
+val stringValue: String? = nullableString as? String
+
+Not-null assertion operator (!!):
+val nullableString: String? = "Hello"
+val nonNullString: String = nullableString!! // This line asserts that nullableString is not null
