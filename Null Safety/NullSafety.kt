709b90936d30c6a138793944ca59f41c6343f4e5
Abhishek kumar <abhishekhzb8@gmail.com>
2024-05-09 21:19:42 +0530

    Update NullSafety.kt

--- Null Safety/NullSafety.kt
+++ Null Safety/NullSafety.kt
@@ -1,8 +1,17 @@
 Safe Calls (?.):  
 val length: Int? = nullableString?.length
 
+//..............................................................................
 Elvis Operator (?:): 
 val nonNullString: String = nullableString ?: "Default Value"
+
+fun main() {
+  val nameApi = null
+  val nameApi = "Nabajyoti"
+  val name: String = nameApi ?: "Anynomous"
+  println(name)
+}
+//..............................................................................
 
 Safe Cast Operator (as?): 
 val stringValue: String? = nullableString as? String
