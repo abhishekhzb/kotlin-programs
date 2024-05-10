87560276e8a504e3c84c0d56536b39490d538645
Abhishek kumar <abhishekhzb8@gmail.com>
2024-05-10 17:11:11 +0530

    Update Flow.kt

--- Flow/Flow.kt
+++ Flow/Flow.kt
@@ -1,1 +1,14 @@
+// Flow
+fun main() = runBlocking<Unit> {
+    val numbersFlow = flow {
+        for (i in 1..3) {
+            emit(i)
+        }
+    }
+
+    numbersFlow.collect { value ->
+        println(value)
+    }
+}
+
 
