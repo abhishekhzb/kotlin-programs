6be89f496aaec052e73086a86ac501dbec700637
Abhishek kumar <abhishekhzb8@gmail.com>
2024-05-22 08:19:47 +0530

    Update Activity.kt

--- MVVM/Activity.kt
+++ MVVM/Activity.kt
@@ -1,1 +1,34 @@
+class MainActivity : AppCompatActivity() {
+    
+    private lateinit var userViewModel: UserViewModel
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        // Get the UserDao from the database
+        val userDao = AppDatabase.getDatabase(application).userDao()
+        
+        // Create an instance of the Retrofit API service
+        val apiService = RetrofitInstance.apiService
+        
+        // Create the UserRepository
+        val repository = UserRepository(userDao, apiService)
+        
+        // Create the ViewModelFactory
+        val viewModelFactory = UserViewModelFactory(repository)
+        
+        // Obtain the ViewModel
+        userViewModel = ViewModelProvider(this, viewModelFactory).get(UserViewModel::class.java)
+
+        // Observe the LiveData from the ViewModel
+        userViewModel.allUsers.observe(this, Observer { users ->
+            // Update UI
+        })
+
+        findViewById<Button>(R.id.button_add_user).setOnClickListener {
+            userViewModel.fetchUserFromApi()
+        }
+    }
+}
 
