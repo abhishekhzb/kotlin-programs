54667425e75eb6bf7a526c87aa30bc899cef968a
Abhishek kumar <abhishekhzb8@gmail.com>
2025-07-05 15:30:37 +0530

    Update Anagram.kt

--- Anagram.kt
+++ Anagram.kt
@@ -1,33 +1,21 @@
-
 fun main() {
-    val s1 = "listen"
-    val s2 = "silent"
+    val s1 = "silent"
+    val s2 = "listen"
 
-    val result = checkAnagram(s1, s2)
-    println(result)
-}
-
-fun checkAnagram(s1: String, s2: String): Boolean {
-    val s1Arr = s1.toCharArray()
-    val s2Arr = s2.toCharArray()
-
-    if (s1Arr.size != s2Arr.size) {
-        return false
+    if (areAnagrams(s1, s2)) {
+        println("Anagram")
+    } else {
+        println("Not Anagram")
     }
-
-    s1Arr.sort()
-    s2Arr.sort()
-
-    return s1Arr.contentEquals(s2Arr)
-}
-
-//....................................................................
-fun main() {
-    val s1 = "listen"
-    val s2 = "silent"
-    println(areAnagrams(s1, s2))
 }
 
 fun areAnagrams(s1: String, s2: String): Boolean {
-    return s1.length == s2.length && s1.toCharArray().sorted() == s2.toCharArray().sorted()
+    if (s1.length != s2.length) {
+        return false
+    }
+
+    val sorted1 = s1.toCharArray().sorted()
+    val sorted2 = s2.toCharArray().sorted()
+
+    return sorted1 == sorted2
 }
