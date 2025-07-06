9dd9b27e96b742d82244ec8945d926f1772dadde
Abhishek kumar <abhishekhzb8@gmail.com>
2025-07-06 16:14:44 +0530

    Update LargestAndSmallestNumberOfArray.kt

--- Array/LargestAndSmallestNumberOfArray.kt
+++ Array/LargestAndSmallestNumberOfArray.kt
@@ -1,1 +1,30 @@
+fun main() {
+    val list = arrayOf(1, 2, 3, 4, 5, 6, 7, 8)
 
+    println("Largest: ${largestNumber(list)}")
+    println("Smallest: ${smallestNumber(list)}")
+}
+
+fun largestNumber(list: Array<Int>): Int {
+    var max = list[0]
+
+    for (num in list) {
+        if (max < num) {
+            max = num
+        }
+    }
+
+    return max
+}
+
+fun smallestNumber(list: Array<Int>): Int {
+    var min = list[0]
+
+    for (num in list) {
+        if (min > num) {
+            min = num
+        }
+    }
+
+    return min
+}
