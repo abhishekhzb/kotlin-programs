018db421212a851741646f85d15bdf4623a645ce
Abhishek kumar <abhishekhzb8@gmail.com>
2025-09-04 21:37:45 +0530

    Update ParallelCoroutine.kt

--- Coroutine/ParallelCoroutine.kt
+++ Coroutine/ParallelCoroutine.kt
@@ -1,42 +1,26 @@
 import kotlinx.coroutines.*
-import kotlin.system.measureTimeMillis
 
-suspend fun apiCall1(): String {
+suspend fun api1(): String {
     delay(1000)
-    return "API 1 Result"
+    return "API 1"
 }
 
-suspend fun apiCall2(): String {
+suspend fun api2(): String {
     delay(1000)
-    return "API 2 Result"
+    return "API 2"
 }
 
-suspend fun apiCall3(): String {
+suspend fun api3(): String {
     delay(1000)
-    return "API 3 Result"
+    return "API 3"
 }
 
 fun main() = runBlocking {
-    val time = measureTimeMillis {
-        // Run all 3 in parallel
-        val job1 = async { apiCall1() }
-        val job2 = async { apiCall2() }
-        val job3 = async { apiCall3() }
+    val job1 = async { api1() }
+    val job2 = async { api2() }
+    val job3 = async { api3() }
 
-        val result1 = job1.await()
-        val result2 = job2.await()
-        val result3 = job3.await()
-
-        println(result1)
-        println(result2)
-        println(result3)
-    }
-
-    println("Time taken: $time ms")
+    println(job1.await())
+    println(job2.await())
+    println(job3.await())
 }
-..........................................
-OUTPUT :
-API 1 Result
-API 2 Result
-API 3 Result
-Time taken: ~1005 ms
